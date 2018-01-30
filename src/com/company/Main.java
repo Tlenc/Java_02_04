809a53ff5918d98c59880b1caae0e1dfb2061ebe
Tlenc <tlenciauskas@gmail.com>
2018-01-30 15:24:17 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Įveskite du skaičius");
+        Scanner sc = new Scanner(System.in);
+        float psk = sc.nextFloat();
+        float ask = sc.nextFloat();
+
+
+        System.out.println("Skirtumas :" + skirtumas(psk,ask));
+        System.out.println("Suma : " + suma(psk,ask));
+    }
+    public static float suma(float psk,float ask){
+        return psk+ask;
+
+    }
+    public static float skirtumas(float psk,float ask){
+       return psk-ask;
+
     }
 }
